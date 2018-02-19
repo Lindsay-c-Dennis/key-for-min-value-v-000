6d4a2e287286325b5075ada1439ceca333a202fc
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-02-19 18:43:44 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,8 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     value << value
   end
-  values.inject do |memo, 
+  values.inject do |memo, value|
+    
     end
   end
   name_hash[low_value]
