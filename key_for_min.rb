41bf401f1b28bc34c60ecab11ad564fbe3aa0c66
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-02-19 18:43:21 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,10 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  collection = []
+  values = []
   name_hash.each do |key, value|
-    if value < low_value
-      low_value = value
+    value << value
+  end
+  values.inject do |memo, 
     end
   end
   name_hash[low_value]
